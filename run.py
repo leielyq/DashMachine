49dbe0f11ecb181f3dba502f0f58cee258de6a36
杨 <leielyq123@gmail.com>
2022-04-04 01:38:49 +0800

    Update run.py

--- run.py
+++ run.py
@@ -20,4 +20,4 @@ from dashmachine.main.utils import dashmachine_init
 dashmachine_init()
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True, host="0.0.0.0", threaded=True)
+    app.run(debug=True, use_reloader=True, host="::", threaded=True)
